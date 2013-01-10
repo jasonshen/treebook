da87b84fba4963512e6d1ffd2376b133a489b972
Jason Shen <jasonyshen@gmail.com>
2013-01-10 11:40:00 -0800

    Trying to add my first commit with status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '37b3910b454dd70e56b9c38799329bf898a4b6f7aa8c895f155f9bfa1969012806b80a7d04c1baa70c197c74a18c06284f5b2bc6c9b4dd36215dc327da2bd341'
