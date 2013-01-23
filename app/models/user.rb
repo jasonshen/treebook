7cfe8dbb03ff693de23324e9238da96c10b8bf26
Jason Shen <jasonyshen@gmail.com>
2013-01-23 13:51:16 -0800

    Added tests for user first name last name and profile correctness and uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,26 @@ class User < ActiveRecord::Base
   				:first_name, :last_name, :profile_name
   # attr_accessible :title, :body
 
+  # defining a full name
+  def full_name
+      first_name + " " + last_name
+  end
+
+# added for making our tests work
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :profile_name, presence: true, 
+                                    uniqueness: true,
+                                    format: {
+                                      with: /^[a-zA-Z0-9_-]+$/,
+                                      message: "must be formatted correctly."
+                                    }
+
+
+
   # has many status
 
   has_many :statuses
 
-  # defining a full name
-  def full_name
-  		first_name + " " + last_name
-  	end
+
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -4,4 +4,45 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "a user should enter a first name" do
+  	user = User.new
+  	assert  !user.save
+  	assert !user.errors[:first_name].empty?
+  end
+
+  test "a user should enter a last name" do
+  	user = User.new
+  	assert  !user.save
+  	assert !user.errors[:last_name].empty?
+  end
+
+  test "a user should enter a profile name" do
+  	user = User.new
+  	assert  !user.save
+  	assert !user.errors[:profile_name].empty?
+  end
+
+  test "a user should have a unique profile name" do
+  	user = User.new
+  	user.profile_name = users(:jason).profile_name
+  	user.email = "jasonshen@gmail.com"
+  	user.first_name = "Jason"
+  	user.last_name = "Shen"
+  	user.encrypted_password = "password"
+
+
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  end
+
+  test "a user should have a profile name without spaces" do	
+  	user = User.new
+  	user.profile_name = "jason shen"
+
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  	assert user.errors[:profile_name].include?("must be formatted correctly.")
+  end
+
 end
